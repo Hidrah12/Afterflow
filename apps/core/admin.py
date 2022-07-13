6072ec7cd4a41e64068389309e28fd7c9ea15464
Dany <danielvillasantioficial@gmail.com>
2022-07-13 19:57:23 -0400

    se crearon los modelos

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Subcategory, Category, TypeCategory, Article
 
-# Register your models here.
+class SubcategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['id', 'name']
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['id', 'name']
+
+class TypeCategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['id', 'name']
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'slug', 'minute_of_reading', 
+    'category_for_development', 'publication_date', 'last_modification']
+    list_display_links = ['id', 'title']
+    list_filter = ['category']
+
+admin.site.register(Subcategory, SubcategoryAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(TypeCategory, TypeCategoryAdmin)
+admin.site.register(Article, ArticleAdmin)
--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+class Subcategory(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self) -> str:
+        return f'{self.name}'
+
+class TypeCategory(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return f'{self.name}'
+
+class Category(models.Model):
+    name = models.CharField(max_length = 30)
+    subcategorys = models.ManyToManyField(Subcategory, null = True, blank = True)
+    type_category = models.ForeignKey(TypeCategory, on_delete = models.PROTECT, null = True)
+
+    def __str__(self) -> str:
+        return f'{self.name}'
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(null = True, blank = True, db_index = True)
+    minute_of_reading = models.CharField(max_length=10, null = True, blank = True)
+    category = models.ForeignKey(Category, on_delete = models.PROTECT)
+    category_for_development = models.CharField('Category Development', max_length=30)
+    summary = models.TextField()
+    publication_date = models.DateTimeField(auto_now_add = True)
+    last_modification = models.DateTimeField(auto_now = True)
+
+    template_name = models.CharField(max_length = 50, null = True, blank = True)
+    featured = models.BooleanField(null = True, blank = True)
+    image = models.ImageField(upload_to = 'featured', null = True)
+    
+    def __str__(self) -> str:
+        return f'{self.title}'
+
+# 1 Django [Vistas, Templates, Forms, Models] framework
+# 2 Python null language
+# 3 MySQL null database
+# 4 PostgreSQL null database
