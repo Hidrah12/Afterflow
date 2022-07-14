e3710f8af6c904bb32d54cd152c5936ec0216d0b
Dany <danielvillasantioficial@gmail.com>
2022-07-14 09:39:42 -0400

    feat: se agrego el menu de sesiones al navbar

---
+++ static/js/menuSession.js
@@ -0,0 +1,9 @@
+function showMenuSession() {
+    let menuSession = document.getElementById('menu-session')
+    if (menuSession.style.display === 'none') {
+        menuSession.style.display = 'block'
+    }
+    else {
+        menuSession.style.display = 'none'
+    }
+}
