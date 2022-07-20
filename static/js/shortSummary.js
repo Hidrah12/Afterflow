096bb29cbde322a753f29e9e26a91cc77976bdbe
Dany <danielvillasantioficial@gmail.com>
2022-07-20 10:49:29 -0400

    se cambio la forma de cortar los parrafos

--- static/js/shortSummary.js
+++ static/js/shortSummary.js
@@ -1,8 +1,6 @@
 let summarys = document.getElementsByClassName('parrafo')
 Array.from(summarys).map(summary => {
     if (summary.innerHTML.length >= 100) {
-        let slice = summary.innerHTML.slice(100,summary.length)
-        let index = slice.lastIndexOf(' ')
-        summary.innerHTML = summary.innerHTML.slice(0, index) + "..." + " <a href='#' class='text-cyan-400'>Leer más</a>"
+        summary.innerHTML = summary.innerHTML.slice(0, 200) + "..." + " <a href='#' class='text-cyan-400'>Leer más</a>"
     }
 })
