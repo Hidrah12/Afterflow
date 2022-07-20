c6161b3ba4bb64446d9daafaaac81232ae8671c3
Dany <danielvillasantioficial@gmail.com>
2022-07-20 10:44:44 -0400

    feat: se realizaron cambios en el recorte del summary

--- static/js/shortSummary.js
+++ static/js/shortSummary.js
@@ -3,7 +3,7 @@ Array.from(summarys).map(summary => {
     if (summary.innerHTML.length >= 100) {
         let slice = summary.innerHTML.slice(100,summary.length)
         alert(slice)
-        let index = slice.lastIndexOf(' ')
+        let index = slice.indexOf(' ')
         summary.innerHTML = summary.innerHTML.slice(0, index) + "..." + " <a href='#' class='text-cyan-400'>Leer más</a>"
     }
 })
