a322430370c04d6a383ae1075c077db7d46d0d43
Dany <danielvillasantioficial@gmail.com>
2022-07-20 10:48:17 -0400

    fix: se arreglo un problema al recortar los parrafos

--- static/js/shortSummary.js
+++ static/js/shortSummary.js
@@ -2,8 +2,7 @@ let summarys = document.getElementsByClassName('parrafo')
 Array.from(summarys).map(summary => {
     if (summary.innerHTML.length >= 100) {
         let slice = summary.innerHTML.slice(100,summary.length)
-        alert(slice)
-        let index = slice.indexOf(' ')
+        let index = slice.lastIndexOf(' ')
         summary.innerHTML = summary.innerHTML.slice(0, index) + "..." + " <a href='#' class='text-cyan-400'>Leer más</a>"
     }
 })
