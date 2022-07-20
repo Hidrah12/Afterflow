e5fdbe7f29b2de7ea9008c357b935fd14c53cc46
Dany <danielvillasantioficial@gmail.com>
2022-07-20 10:05:41 -0400

    style: se cambio el color del enlace Leer mas

--- static/js/shortSummary.js
+++ static/js/shortSummary.js
@@ -3,6 +3,6 @@ Array.from(summarys).map(summary => {
     if (summary.innerHTML.length >= 100) {
         let slice = summary.innerHTML.slice(100,summary.length)
         let index = slice.lastIndexOf(' ')
-        summary.innerHTML = summary.innerHTML.slice(0, index) + "..." + " <a href='#' class='text-cyan-400'>Leer más</a>"
+        summary.innerHTML = summary.innerHTML.slice(0, index) + "..." + " <a href='#' class='text-cyan-300'>Leer más</a>"
     }
 })
