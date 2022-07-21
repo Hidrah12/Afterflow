82b4dac7ade304beb7f0681508eec1d761b7dfb4
Dany <danielvillasantioficial@gmail.com>
2022-07-21 11:01:20 -0400

    style: se cambio el tamaño del enlace leer mas

--- static/js/custom.js
+++ static/js/custom.js
@@ -2,7 +2,7 @@
 let summarys = document.getElementsByClassName('paragraph')
 Array.from(summarys).map(summary => {
     if (summary.innerHTML.length >= 100) {
-        summary.innerHTML = summary.innerHTML.slice(0, 200) + "..." + " <a href='#' class='text-cyan-400 hover:text-cyan-500 text-xl'>Leer más</a>"
+        summary.innerHTML = summary.innerHTML.slice(0, 200) + "..." + " <a href='#' class='text-cyan-400 hover:text-cyan-500 text-base'>Leer más</a>"
     }
 })
 
