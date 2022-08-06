febe1fcbbbee633f6659e1ae52548f9749d3d32b
Dany <danielvillasantioficial@gmail.com>
2022-08-06 11:38:58 -0400

    feat: ahora algunas funciones para los botones son declaradas en el index.js

--- static/js/index.js
+++ static/js/index.js
@@ -3,7 +3,8 @@
         fetch(`/api/set/`)
     }
 )();
-function showMenuSession() {
+function showMenuSession(event) {
+    event.preventDefault()
     let menuSession = document.getElementById('menu-session')
     if (menuSession.style.display === 'none') {
         menuSession.style.display = 'block'
@@ -12,11 +13,13 @@ function showMenuSession() {
         menuSession.style.display = 'none'
     }
 }
+let btnShowMenuSession = document.getElementById('btnShowMenuSession')
+btnShowMenuSession.addEventListener('click', showMenuSession)
 
-function showSidePanel() {
+function showSidePanel(event) {
+    event.preventDefault()
     let sidePanel = document.getElementById('side-panel')
     let block = document.getElementById('block')
-
     if (sidePanel.classList.toString().includes('panel-lateral') ){
         sidePanel.classList.remove('panel-lateral')
         block.style.display = 'none'
@@ -25,6 +28,8 @@ function showSidePanel() {
         block.style.display = 'block'
     }
 }
+let btnShowSidePanel = document.getElementById('btnShowSidePanel')
+btnShowSidePanel.addEventListener('click', showSidePanel)
 
 function showOptionsMenu(optionName) {
     let optionSelected = document.getElementById(optionName)
@@ -41,21 +46,22 @@ function showOptionsMenu(optionName) {
     }
 }
 
-function getMoreArticles() {
+function getMoreArticles(event) {
+    event.preventDefault()
     fetch("/api/")
     .then(response => response.json())
     .then(function (data) {
         data.map(element => {
             if (element['Message']) {
-                let btnGetArticles = document.getElementById('get-articles')
-                btnGetArticles.style.display = 'none'
+                let btnGetMoreArticles = document.getElementById('btnGetMoreArticles')
+                btnGetMoreArticles.style.display = 'none'
             } else {
                 let contentArticles = document.getElementById('content-articles')
                 contentArticles.innerHTML += `
                 <article id="${element.id}" class="article last:mb-10 my-24 relative">
                     <h2 class="my-5 "><a href="#" class="title text-5xl sm:text-3xl font-extrabold font-['Roboto'] my-5 hover:text-cyan-400 transition-colors dark:text-slate-50 dark:hover:text-cyan-400 xr:text-[1.3rem]" title="${element.title}">${element.title}</a></h2>
                     <span class="italic text-[rgb(172,172,172)]">Publicado el ${element.publication_date} <a href="" class=" py-1 p-2 ml-2 rounded-sm text-xs font-normal bg-[#f3f1f1d4] text-slate-600 hover:bg-[#d8d8d8]">${element.category_for_development}</a></span>
-                    <p class='paragraph text-xl sm:text-base leading-8 font-light my-5 font-["Roboto"] text-slate-800 dark:text-slate-300 whitespace-pre-wrap'>${element.summary}</p>
+                    <p class='paragraph text-xl sm:text-base leading-8 text-justify font-light my-5 font-["Roboto"] text-slate-800 dark:text-slate-300 whitespace-pre-wrap'>${element.summary}</p>
                 </article>
                 `
             }
@@ -68,3 +74,5 @@ function getMoreArticles() {
         })
     })
 }
+let btnGetMoreArticles = document.getElementById('btnGetMoreArticles')
+btnGetMoreArticles.addEventListener('click', getMoreArticles)
