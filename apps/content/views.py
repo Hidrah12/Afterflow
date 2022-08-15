a8a9f4b6730da6c290d62cc05533e19898540c11
Dany <danielvillasantioficial@gmail.com>
2022-08-14 20:24:09 -0400

    style: se realizaron cambios al momento de leer un articulo

--- apps/content/views.py
+++ apps/content/views.py
@@ -3,4 +3,5 @@ from apps.core.models import Article
 
 def item_details_view(request, slug):
     article = Article.objects.get(slug = slug)
-    return render(request, f'public/articles/{article.template_name}', {'article': article})
+    recommend = Article.objects.filter(category = article.category)
+    return render(request, f'public/articles/{article.template_name}', {'article': article,'recommend': recommend})
