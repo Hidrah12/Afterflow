9c17f6c5c002c586ff39d07f66e4d212ca6dbca9
Dany <danielvillasantioficial@gmail.com>
2022-08-14 20:31:14 -0400

    feat: se elimino los articulos recomendados

--- apps/content/views.py
+++ apps/content/views.py
@@ -3,5 +3,4 @@ from apps.core.models import Article
 
 def item_details_view(request, slug):
     article = Article.objects.get(slug = slug)
-    recommend = Article.objects.filter(category = article.category)
-    return render(request, f'public/articles/{article.template_name}', {'article': article,'recommend': recommend})
+    return render(request, f'public/articles/{article.template_name}', {'article': article})
