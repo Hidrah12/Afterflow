3c69b7439c071d7b42af3d70a8feab61624bd9a0
Dany <danielvillasantioficial@gmail.com>
2022-08-23 11:45:30 -0400

    feat: se creo el articulo1

--- static/js/custom.js
+++ static/js/custom.js
@@ -9,6 +9,20 @@ Array.from(summarys).forEach(summary => {
         summary.innerHTML = summary.innerHTML.slice(0, index) + "..." + ` <a href='#' class='read-more text-cyan-400 hover:text-cyan-500 text-base'>Leer más</a>`
     }
 })
+// Se agrego el nombre a los titulos h2, h3 y los párrafos.
+let ctnSummary = document.getElementsByTagName('p')
+Array.from(ctnSummary).forEach(element => {
+    element.className = 'ctn_summary'
+})
+let ctnTitles3 = document.getElementsByTagName('h3')
+Array.from(ctnTitles3).forEach(element => {
+    element.className = 'ctn_title3'
+})
+let ctnTitles2 = document.getElementsByTagName('h2')
+Array.from(ctnTitles2).forEach(element => {
+    element.className = 'ctn_title2'
+})
+// Se agrego las url a los enlaces de leer más.
 let urls = []
 let titles = document.getElementsByClassName('title')
 Array.from(titles).forEach(element => {
