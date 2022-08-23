45a38d42a7a6ca64f3e6752440901d3de0e137ed
Dany <danielvillasantioficial@gmail.com>
2022-08-22 20:31:23 -0400

    feat: se agrego nuevo contenido

--- apps/content/views.py
+++ apps/content/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from apps.core.models import Article
 from django.views.decorators.cache import cache_page
 
-@cache_page(60 * 10)
+
 def item_details_view(request, slug):
     article = Article.objects.get(slug = slug)
     return render(request, f'public/articles/{article.template_name}', {'article': article})
